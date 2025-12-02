4397a3434a4f0b855f2dc9eddeb5b22f4e762af0
Javier <javier@eskura21.com>
2025-12-02 10:25:57 +0100

    Ejemplo básico de fetch con fichero local de JSON

---
+++ FRONTEND/HTML_CSS_JAVASCRIPT/js/productos.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const URL = 'json/productos.json';
+
+globalThis.addEventListener('DOMContentLoaded', async () => {
+    const respuesta = await fetch(URL);
+    const productos = await respuesta.json();
+    
+    const tbody = document.querySelector('tbody');
+
+    for(const producto of productos) {
+        const tr = document.createElement('tr');
+
+        tr.innerHTML = `
+            <th>${producto.id}</th>
+            <td>${producto.nombre}</td>
+            <td>${producto.precio}</td>
+            <td>
+                <a href="#">Editar</a>
+                <a href="#">Borrar</a>
+            </td>
+        `;
+
+        tbody.appendChild(tr);
+    }
+});
