8b86e9856a5c17b3778f0c27db1fa816e7ac93a4
Javier <javier@eskura21.com>
2025-12-04 09:35:20 +0100

    Versión jQuery de productos

--- FRONTEND/JQUERY/js/productos.js
+++ FRONTEND/JQUERY/js/productos.js
@@ -1,135 +1,119 @@
+/* eslint-disable */
+
 'use strict';
 
-const URL = 'http://localhost:3000/productos';
+var URL = 'http://localhost:3000/productos';
 
-let formulario, tabla;
-
-globalThis.addEventListener('DOMContentLoaded', async () => {
-    formulario = document.forms[0];
-    tabla = document.querySelector('table');
-
+$(function () {
     mostrarTabla();
 });
 
 function anyadir() {
     console.log('Añadir');
 
-    formulario.reset();
+    $('form')[0].reset();
 
     mostrarFormulario();
 }
 
-async function editar(id) {
+function editar(id) {
     console.log('Editar', id);
 
-    try {
-        const response = await fetch(`${URL}/${id}`);
-        const producto = await response.json();
-
+    $.getJSON(URL + '/' + id, function (producto) {
         console.log(producto);
 
-        formulario.id.value = producto.id;
-        formulario.nombre.value = producto.nombre;
-        formulario.precio.value = producto.precio;
+        $('#id').val(producto.id);
+        $('#nombre').val(producto.nombre);
+        $('#precio').val(producto.precio);
 
         mostrarFormulario();
-    } catch (error) {
+    }).fail(function (jqXHR, error, errorThrown) {
         alert('Ha habido un error al recibir los datos del producto');
 
         console.error(error);
-    }
+    });
 }
 
-async function borrar(id) {
+function borrar(id) {
     console.log('Borrar', id);
 
     if (!confirm('¿Estás seguro de que quieres borrar el producto id ' + id + '?')) {
         return;
     }
 
-    try {
-        const response = await fetch(`${URL}/${id}`, { method: 'DELETE' });
+    $.ajax(URL + '/' + id, { method: 'DELETE' })
+        .done(function (data, textStatus, jqXHR) {
+            console.log(textStatus);
 
-        console.log(response.statusText);
-
-        mostrarTabla();
-    } catch (error) {
-        console.error(error);
-    }
+            mostrarTabla();
+        })
+        .fail(function (jqXHR, error, errorThrown) {
+            console.error(error);
+        });
 }
 
-async function guardar() {
-    const producto = {
-        nombre: formulario.nombre.value,
-        precio: formulario.precio.value,
+function guardar() {
+    var producto = {
+        nombre: $('#nombre').val(),
+        precio: $('#precio').val(),
     };
 
-    const id = formulario.id.value;
+    var id = $('#id').val();
 
-    let url, metodo;
+    var url, metodo;
 
     if (id) {
         producto.id = id;
 
-        url = `${URL}/${id}`;
+        url = URL + '/' + id;
         metodo = 'PUT';
     } else {
         url = URL;
         metodo = 'POST';
     }
 
-    try {
-        const response = await fetch(url, {
-            method: metodo,
-            headers: { 'content-type': 'application/json' },
-            body: JSON.stringify(producto),
+    $.ajax(url, {
+        method: metodo,
+        headers: { 'content-type': 'application/json' },
+        data: JSON.stringify(producto),
+    })
+        .done(function (data) {
+            console.log(data);
+            mostrarTabla();
+        }).fail(function (jqXHR, error, errorThrown) {
+            console.error(error);
         });
-
-        const data = await response.json();
-
-        console.log(data);
-    } catch (error) {
-        console.error(error);
-    }
-
-    mostrarTabla();
 }
-
-
 
 function mostrarFormulario() {
-    formulario.style.display = null;
-    tabla.style.display = 'none';
+    $('table').hide();
+    $('form').show();
 }
 
-async function mostrarTabla() {
-    await recargarTabla();
+function mostrarTabla() {
+    recargarTabla();
 
-    tabla.style.display = null;
-    formulario.style.display = 'none';
+    $('table').show();
+    $('form').hide();
 }
 
-async function recargarTabla() {
-    const respuesta = await fetch(URL);
-    const productos = await respuesta.json();
+function recargarTabla() {
+    $.getJSON(URL, function (productos) {
+        $('tbody').empty();
 
-    const tbody = document.querySelector('tbody');
-
-    tbody.innerHTML = '';
-
-    for (const producto of productos) {
-        const tr = document.createElement('tr');
-
-        tr.innerHTML = `
-            <th>${producto.id}</th>
-            <td>${producto.nombre}</td>
-            <td>${producto.precio}</td>
-            <td>
-                <a href="javascript:editar(${producto.id})">Editar</a>
-                <a href="javascript:borrar(${producto.id})">Borrar</a>
-            </td>
-        `;
-
-        tbody.appendChild(tr);
-    }
+        $.each(productos, function () {
+            $('<tr>')
+                .html(
+                    '<th>' + this.id + '</th>' +
+                    '<td>' + this.nombre + '</td>' +
+                    '<td>' + this.precio + '</td>' +
+                    '<td>' +
+                    '<a href="javascript:editar(' + this.id + ')">Editar</a>' +
+                    '<a href="javascript:borrar(' + this.id + ')">Borrar</a>' +
+                    '</td>')
+                .appendTo('tbody');
+        });
+    });
 }
+
+/* eslint-enable */
