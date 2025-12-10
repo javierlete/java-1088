f25a2269c358ba5aef1c83cdf76a46cfe0daf2e5
Javier <javier@eskura21.com>
2025-12-10 09:14:50 +0100

    Eventos y estado

--- FRONTEND/REACT/js/index.jsx
+++ FRONTEND/REACT/js/index.jsx
@@ -1,10 +1,15 @@
+function valorCambiado(valor) {
+    console.log(valor);
+}
+
 function App() {
+
     return (
         <main>
             <Titulo texto="Hola a todos" />
             <Titulo texto="Hola" />
 
-            <LabelInput id="nombre" etiqueta="Nombre" tipo="text" />
+            <LabelInput id="nombre" etiqueta="Nombre" tipo="text" valor="Javier" onValorChange={valorCambiado} />
             <LabelInput id="email" etiqueta="Email" tipo="email" />
 
             <Bolitas datos={['Uno', 'Dos', 'Tres', 'Cuatro']} />
@@ -20,11 +25,19 @@ function Titulo({ texto }) {
     return <h1>{texto}</h1>;
 }
 
-function LabelInput({ id, etiqueta, tipo }) {
+function LabelInput({ id, etiqueta, tipo, valor, onValorChange }) {
+    const [estado, setEstado] = React.useState(valor);
+
+    function onChange(e) {
+        setEstado(e.target.value);
+
+        onValorChange(e.target.value);
+    }
+
     return (
         <div>
             <label for={id}>{etiqueta}</label>
-            <input id={id} type={tipo}></input>
+            <input id={id} type={tipo} value={estado} onChange={onChange}></input>
         </div>
     );
 }
