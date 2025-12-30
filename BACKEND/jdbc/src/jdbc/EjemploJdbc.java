ac5e9f361405dbdf30faa347f0be79802c877336
'Javier <'javier@eskura21.com'>
2025-12-30 10:47:05 +0100

    Ejemplo comentarios

--- BACKEND/jdbc/src/jdbc/EjemploJdbc.java
+++ BACKEND/jdbc/src/jdbc/EjemploJdbc.java
@@ -2,24 +2,41 @@ package jdbc;
 
 import java.sql.*;
 
+/**
+ * Clase de ejemplo de <code>JDBC</code>
+ * 
+ * @author Javier Lete
+ */
 public class EjemploJdbc {
+	/**
+	 * Método de entrada para cualquier aplicación de Java
+	 * @param args Los argumentos de la consola
+	 * @throws SQLException En este caso lanzamos esta excepción porque no gestionamos a nivel interno la misma
+	 */
 	public static void main(String[] args) throws SQLException {
-		String url = "jdbc:sqlite:jdbc.db";
+		/*
+		 * En este ejemplo vamos a mostrar
+		 * cómo conectar con una base de datos con JDBC
+		 */
+		
+		String url = "jdbc:sqlite:jdbc.db"; // Definimos la URL
 		String user = "";
 		String pass = "";
+		
+		Connection con = DriverManager.getConnection(url, user, pass); // Conectamos con la base de datos
+		Statement st = con.createStatement(); // Creamos una sentencia
 
-		Connection con = DriverManager.getConnection(url, user, pass);
-		Statement st = con.createStatement();
-
-		st.executeUpdate("drop table if exists personas");
-		st.executeUpdate("create table personas (id integer primary key, nombre varchar(30))");
+		st.executeUpdate("drop table if exists personas"); // Borramos la tabla si existiera
+		st.executeUpdate("create table personas (id integer primary key, nombre varchar(30))"); // Creamos una nueva tabla
+		
+		// Insertamos registros
 		st.executeUpdate("insert into personas (nombre) values ('leo')");
 		st.executeUpdate("insert into personas (nombre) values ('yui')");
 
-		ResultSet rs = st.executeQuery("select * from personas");
+		ResultSet rs = st.executeQuery("select * from personas"); // Pedimos todos los registros
 
-		while (rs.next()) {
-			System.out.printf("%5s %-20s\n", rs.getString("id"), rs.getString("nombre"));
+		while (rs.next()) { // Mientras haya registros pasamos al siguiente...
+			System.out.printf("%5s %-20s\n", rs.getString("id"), rs.getString("nombre")); // ...y mostramos su información
 		}
 	}
 }
