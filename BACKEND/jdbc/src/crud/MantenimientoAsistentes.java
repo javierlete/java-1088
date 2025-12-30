657be94e4de7a646ba60df4c4de80e59803a8141
'Javier <'javier@eskura21.com'>
2025-12-30 10:49:20 +0100

    (Formateado del código)

--- BACKEND/jdbc/src/crud/MantenimientoAsistentes.java
+++ BACKEND/jdbc/src/crud/MantenimientoAsistentes.java
@@ -9,20 +9,20 @@ import java.util.Scanner;
 
 public class MantenimientoAsistentes {
 	private static final Scanner SC = new Scanner(System.in);
-	
+
 	private static final String URL = "jdbc:sqlite:asistentes.db"; // Definimos la URL
 	private static final String USER = "";
 	private static final String PASS = "";
-	
+
 	private static Connection con;
-	private static Statement st; 
-	
+	private static Statement st;
+
 	public static void main(String[] args) throws SQLException {
 		con = DriverManager.getConnection(URL, USER, PASS); // Conectamos con la base de datos
 		st = con.createStatement(); // Creamos una sentencia
-		
+
 		String opcion;
-		
+
 		do {
 			mostrarMenu();
 			opcion = pedirOpcion();
@@ -34,35 +34,35 @@ public class MantenimientoAsistentes {
 		System.out.println("""
 				MENU
 				====
-				
+
 				1. LISTADO
 				2. BUSCAR POR ID
-				
+
 				3. INSERTAR
 				4. MODIFICAR
 				5. BORRAR
-				
+
 				0. SALIR
 				""");
 	}
 
 	private static String pedirOpcion() {
 		System.out.println("Dime la opción que quieres");
-		
+
 		return SC.nextLine();
 	}
 
 	private static void procesarOpcion(String opcion) throws SQLException {
-		switch(opcion) {
+		switch (opcion) {
 		case "1" -> listado();
 		case "2" -> buscarPorId();
-		
+
 		case "3" -> insertar();
 		case "4" -> modificar();
 		case "5" -> borrar();
-		
+
 		case "0" -> salir();
-		
+
 		default -> error();
 		}
 	}
@@ -74,22 +74,27 @@ public class MantenimientoAsistentes {
 		System.out.printf("%5s %-15s %-30s %-40s\n", "==", "======", "=========", "=====");
 
 		while (rs.next()) { // Mientras haya registros pasamos al siguiente...
-			System.out.printf("%5s %-15s %-30s %-40s\n", rs.getString("id"), rs.getString("nombre"), rs.getString("apellidos"), rs.getString("notas") != null ? rs.getString("notas") : "SIN RELLENAR"); // ...y mostramos su información
+			System.out.printf("%5s %-15s %-30s %-40s\n", rs.getString("id"), rs.getString("nombre"),
+					rs.getString("apellidos"), rs.getString("notas") != null ? rs.getString("notas") : "SIN RELLENAR"); // ...y
+																														// mostramos
+																														// su
+																														// información
 		}
 	}
 
 	private static void buscarPorId() throws SQLException {
 		System.out.println("Dime el id");
-		
+
 		int id = Integer.parseInt(SC.nextLine());
-		
+
 		ResultSet rs = st.executeQuery("SELECT * FROM asistentes WHERE id=" + id);
 
 		System.out.printf("%5s %-15s %-30s %-40s\n", "ID", "Nombre", "Apellidos", "Notas");
 		System.out.printf("%5s %-15s %-30s %-40s\n", "==", "======", "=========", "=====");
 
 		while (rs.next()) { // Mientras haya registros pasamos al siguiente...
-			System.out.printf("%5s %-15s %-30s %-40s\n", rs.getString("id"), rs.getString("nombre"), rs.getString("apellidos"), rs.getString("notas")); // ...y mostramos su información
+			System.out.printf("%5s %-15s %-30s %-40s\n", rs.getString("id"), rs.getString("nombre"),
+					rs.getString("apellidos"), rs.getString("notas")); // ...y mostramos su información
 		}
 	}
 
@@ -99,9 +104,9 @@ public class MantenimientoAsistentes {
 
 		System.out.print("Apellidos: ");
 		String apellidos = SC.nextLine();
-		
-		st.executeUpdate("INSERT INTO asistentes (nombre, apellidos) VALUES ('" + nombre + "', '"+ apellidos +"')");
-		
+
+		st.executeUpdate("INSERT INTO asistentes (nombre, apellidos) VALUES ('" + nombre + "', '" + apellidos + "')");
+
 		System.out.println("Insertado");
 	}
 
