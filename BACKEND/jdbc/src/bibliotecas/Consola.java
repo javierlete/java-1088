b70ec389c7a08a8f3c81d30729c028828d32b0ad
'Javier <'javier@eskura21.com'>
2026-01-02 09:56:01 +0100

    Consola con comprobación de enteros al leer

--- BACKEND/jdbc/src/bibliotecas/Consola.java
+++ BACKEND/jdbc/src/bibliotecas/Consola.java
@@ -4,25 +4,31 @@ import java.util.Scanner;
 
 public class Consola {
 	private static final Scanner SC = new Scanner(System.in);
-	
+
 	public static void pl(String mensaje) {
 		System.out.println(mensaje);
 	}
-	
+
 	public static void p(String mensaje) {
 		System.out.print(mensaje);
 	}
-	
+
 	public static void pf(String mensaje, Object... parametros) {
 		System.out.printf(mensaje, parametros);
 	}
-	
+
 	public static String leerString(String mensaje) {
 		p(mensaje + ": ");
-		return SC.nextLine(); 
+		return SC.nextLine();
 	}
-	
+
 	public static int leerInt(String mensaje) {
-		return Integer.parseInt(leerString(mensaje));
+		do {
+			try {
+				return Integer.parseInt(leerString(mensaje));
+			} catch (NumberFormatException e) {
+				pl("No se ha introducido un número");
+			}
+		} while (true);
 	}
 }
