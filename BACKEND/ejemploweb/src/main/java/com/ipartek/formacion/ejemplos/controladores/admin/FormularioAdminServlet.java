b256527232918bcbc74fbec3db5fde12794fdd7e
'Javier <'javier@eskura21.com'>
2026-01-19 09:58:53 +0100

    Añadir funcional

---
+++ BACKEND/ejemploweb/src/main/java/com/ipartek/formacion/ejemplos/controladores/admin/FormularioAdminServlet.java
@@ -0,0 +1,28 @@
+package com.ipartek.formacion.ejemplos.controladores.admin;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/formulario")
+public class FormularioAdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Recoger la información recibida en la petición
+		// Convertir las partes que sean necesarias
+		// Crear objetos con todas las partes
+		// Ejecutar lógica de negocio
+		// Empaquetar modelo para la siguiente vista
+		// Saltar a la siguiente vista
+		
+		request.getRequestDispatcher("/WEB-INF/vistas/admin/formulario.jsp").forward(request, response);
+	}
+
+}
---
+++ BACKEND/ejemploweb/src/main/webapp/WEB-INF/vistas/admin/formulario.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="jakarta.tags.core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Listado de administración de asistentes</title>
+</head>
+<body>
+
+	<form>
+		<input placeholder="Nombre">
+		<input placeholder="Apellidos">
+		
+		<button>Guardar</button>
+	</form>
+
+</body>
+</html>
