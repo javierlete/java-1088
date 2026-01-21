20204ca5ea193ade4b87caec8734d1fc6978f9f1
'Javier <'javier@eskura21.com'>
2026-01-21 09:33:26 +0100

    Creamos un controlador frontal para todas las JSP

---
+++ ipartube/src/main/java/com/ipartek/formacion/ejemplos/ipartube/controladores/ControladorFrontalServlet.java
@@ -0,0 +1,23 @@
+package com.ipartek.formacion.ejemplos.ipartube.controladores;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/cf/*")
+public class ControladorFrontalServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/vistas/" + request.getPathInfo() + ".jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
