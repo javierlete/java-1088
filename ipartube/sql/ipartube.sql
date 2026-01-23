c54bf87351fb7d2ff0aaefa70d7a580950b94d5a
'Javier <'javier@eskura21.com'>
2026-01-23 10:27:09 +0100

    Login real basado en base de datos

--- ipartube/sql/ipartube.sql
+++ ipartube/sql/ipartube.sql
@@ -18,6 +18,32 @@ USE `ipartube`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'javier@email.net','javier'),(2,'pepe@email.net','pepe');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `videos`
 --
 
@@ -34,7 +60,7 @@ CREATE TABLE `videos` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `videoUrl_UNIQUE` (`video_url`),
   UNIQUE KEY `imagenUrl_UNIQUE` (`imagen_url`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +69,7 @@ CREATE TABLE `videos` (
 
 LOCK TABLES `videos` WRITE;
 /*!40000 ALTER TABLE `videos` DISABLE KEYS */;
-INSERT INTO `videos` VALUES (1,'Michael Jackson en Auckly','Con efectos 3D de la época','2026-01-21 10:46:00','https://i.ytimg.com/vi/ChrLRauOR28/hq720.jpg','https://www.youtube.com/embed/ChrLRauOR28'),(2,'Clásicos del Rock',NULL,NULL,'https://i.ytimg.com/vi/4_O-y3SM3UM/hq720.jpg','https://www.youtube.com/embed/4_O-y3SM3UM');
+INSERT INTO `videos` VALUES (1,'Michael Jackson en Auckly','Con efectos 3D de la época.\r\nComo se nota la diferencia...','2026-01-22 10:26:00','https://i.ytimg.com/vi/ChrLRauOR28/hq720.jpg','https://www.youtube.com/embed/ChrLRauOR28'),(3,'Clásicos del Rock','',NULL,'https://i.ytimg.com/vi/4_O-y3SM3UM/hq720.jpg','https://www.youtube.com/embed/4_O-y3SM3UM');
 /*!40000 ALTER TABLE `videos` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -56,4 +82,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-01-22  9:20:52
+-- Dump completed on 2026-01-23 10:26:47
--- ipartube/src/main/java/com/ipartek/formacion/ejemplos/ipartube/accesodatos/UsuarioCrud.java
+++ ipartube/src/main/java/com/ipartek/formacion/ejemplos/ipartube/accesodatos/UsuarioCrud.java
@@ -1,14 +1,33 @@
 package com.ipartek.formacion.ejemplos.ipartube.accesodatos;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import com.ipartek.formacion.ejemplos.ipartube.modelos.Usuario;
 
 public class UsuarioCrud {
 	public static Usuario obtenerPorEmail(String email) {
-		if("javier@email.net".equals(email)) {
-			return new Usuario(1L, "javier@email.net", "contra");
+		try (PreparedStatement pst = JdbcHelper.prepararSql("select * from usuarios where email=?");) {
+			pst.setString(1, email);
+
+			try (ResultSet rs = pst.executeQuery()) {
+				Usuario usuario = null;
+
+				if (rs.next()) {
+					String sId = rs.getString("id");
+					String password = rs.getString("password");
+					
+					Long id = Long.parseLong(sId);
+
+					usuario = new Usuario(id, email, password);
+				}
+
+				return usuario;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
 		}
-		
-		return null;
 	}
 
 }
