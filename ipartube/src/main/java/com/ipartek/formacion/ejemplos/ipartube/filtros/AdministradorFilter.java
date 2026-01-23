fbb551e918e5763758df4a47af2d5ed0ad9babd9
'Javier <'javier@eskura21.com'>
2026-01-23 10:07:56 +0100

    Filtrado de peticiones a /cf/admin/* sólo para usuarios logueados

---
+++ ipartube/src/main/java/com/ipartek/formacion/ejemplos/ipartube/filtros/AdministradorFilter.java
@@ -0,0 +1,33 @@
+package com.ipartek.formacion.ejemplos.ipartube.filtros;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebFilter("/cf/admin/*")
+public class AdministradorFilter extends HttpFilter {
+	private static final long serialVersionUID = 2792678868543353992L;
+
+	@Override
+	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpSession session = request.getSession();
+
+		String email = (String) session.getAttribute("email");
+
+		if (email == null) {
+			response.sendRedirect(request.getContextPath() + "/cf/login");
+			return;
+		}
+
+		super.doFilter(request, response, chain); // Damos permiso a la petición para continuar
+	}
+
+}
