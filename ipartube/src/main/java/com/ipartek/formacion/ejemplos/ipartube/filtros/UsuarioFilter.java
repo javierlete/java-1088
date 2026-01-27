97f773ee7b275db1aa37a6fc502eb9d8a33bce08
'Javier <'javier@eskura21.com'>
2026-01-27 11:04:14 +0100

    Autorizaciones para usuarios

---
+++ ipartube/src/main/java/com/ipartek/formacion/ejemplos/ipartube/filtros/UsuarioFilter.java
@@ -0,0 +1,35 @@
+package com.ipartek.formacion.ejemplos.ipartube.filtros;
+
+import java.io.IOException;
+
+import com.ipartek.formacion.ejemplos.ipartube.modelos.Usuario;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebFilter("/cf/usuario/*")
+public class UsuarioFilter extends HttpFilter {
+	private static final long serialVersionUID = 2792678868543353992L;
+
+	@Override
+	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpSession session = request.getSession();
+
+		Usuario usuario = (Usuario) session.getAttribute("usuario");
+
+		if (!"/usuario".equals(request.getPathInfo()) && usuario == null) {
+			response.sendRedirect(request.getContextPath() + "/cf/login");
+			return;
+		}
+
+		super.doFilter(request, response, chain); // Damos permiso a la petición para continuar
+	}
+
+}
