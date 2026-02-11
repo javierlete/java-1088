85929acfa953fe4ba14477152fbc062b9273d90b
'Javier <'javier@eskura21.com'>
2026-02-11 10:57:54 +0100

    Aspecto Bootstrap

--- BACKEND/ejemplowebmaven/src/main/webapp/js/index.js
+++ BACKEND/ejemplowebmaven/src/main/webapp/js/index.js
@@ -12,53 +12,65 @@ async function videos() {
 
     console.log(videos);
 
-    const dl = document.querySelector('dl');
+    const tarjetas = document.querySelector('#tarjetas-videos');
 
-	dl.innerHTML = '';
-	
+    tarjetas.innerHTML = '';
+
     for (const video of videos) {
-        const dt = document.createElement('dt');
-        const dd = document.createElement('dd');
+        const tarjeta = document.createElement('div');
+		tarjeta.className = 'col';
 
-        dt.innerText = video.titulo;
+        tarjeta.innerHTML = `
+			<div class="card h-100">
+				<div class="ratio ratio-16x9">
+					<img src="${video.imagenUrl}" class="card-img-top" alt="...">
+				</div>
+				<div class="card-body">
+					<h5 class="card-title">
+						${video.titulo}
+					</h5>
+					<a class="btn btn-primary stretched-link"
+						href="javascript:video(${video.id})">Ver video ${video.titulo}</a>
+				</div>
+				<div class="card-footer" style="z-index: 2">
+					<small
+						class="text-body-secondary d-flex justify-content-between align-items-baseline">
+						${video.fecha} ${video.usuarioNombre}
+					</small>
+				</div>
+			</div>
 
-        dd.innerHTML = `
-			<img width="100" src="${video.imagenUrl}">
-			<a href="javascript:video(${video.id})">Ver video</a>
-			<p>${video.usuarioNombre}</p>
-			<p>${video.fecha}</p>
 		`;
 
-        dl.appendChild(dt);
-        dl.appendChild(dd);
+        tarjetas.appendChild(tarjeta);
     }
-	
-	mostrar('listado');
+
+    mostrar('listado');
 }
 
 async function video(id) {
     const respuesta = await fetch(URL + id);
     const video = await respuesta.json();
-	
-	console.log(video);
-	
-	document.getElementById('titulo').innerText = video.titulo;
-	document.getElementById('reproductor').src = video.videoUrl;
-	document.getElementById('usuario').innerText = video.usuarioNombre;
-	document.getElementById('fecha').innerText = video.fecha;
-	document.getElementById('descripcion').innerText = video.descripcion;
-	
-	mostrar('video');	
+
+    console.log(video);
+
+    document.getElementById('titulo').innerText = video.titulo;
+    document.getElementById('reproductor').src = video.videoUrl;
+    document.getElementById('usuario').innerText = video.usuarioNombre;
+    document.getElementById('fecha').innerText = video.fecha;
+    document.getElementById('descripcion').innerText = video.descripcion;
+
+    mostrar('video');
 }
 
 function mostrar(id) {
-	const secciones = document.querySelectorAll('section');
-	
-	for(const seccion of secciones) {
-		if(seccion.id === id) {
-			seccion.style.display = null;
-		} else {
-			seccion.style.display = 'none';
-		}
-	}
+    const secciones = document.querySelectorAll('section');
+
+    for (const seccion of secciones) {
+        if (seccion.id === id) {
+            seccion.style.display = null;
+        } else {
+            seccion.style.display = 'none';
+        }
+    }
 }
