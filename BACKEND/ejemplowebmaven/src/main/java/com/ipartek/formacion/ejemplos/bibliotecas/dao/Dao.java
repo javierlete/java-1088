034a07f80939972d6127e3da13328999e48178eb
'Javier <'javier@eskura21.com'>
2026-03-02 09:48:48 +0100

    Implementación por defecto en el Dao

--- BACKEND/ejemplowebmaven/src/main/java/com/ipartek/formacion/ejemplos/bibliotecas/dao/Dao.java
+++ BACKEND/ejemplowebmaven/src/main/java/com/ipartek/formacion/ejemplos/bibliotecas/dao/Dao.java
@@ -3,10 +3,23 @@ package com.ipartek.formacion.ejemplos.bibliotecas.dao;
 import java.util.Optional;
 
 public interface Dao<T> {
-	Iterable<T> obtenerTodos();
-	Optional<T> obtenerPorId(Long id);
-	
-	T insertar(T objeto);
-	T modificar(T objeto);
-	void borrar(Long id);
+	default Iterable<T> obtenerTodos() {
+		throw new UnsupportedOperationException("NO IMPLEMENTADO");
+	};
+
+	default Optional<T> obtenerPorId(Long id) {
+		throw new UnsupportedOperationException("NO IMPLEMENTADO");
+	};
+
+	default T insertar(T objeto) {
+		throw new UnsupportedOperationException("NO IMPLEMENTADO");
+	};
+
+	default T modificar(T objeto) {
+		throw new UnsupportedOperationException("NO IMPLEMENTADO");
+	};
+
+	default void borrar(Long id) {
+		throw new UnsupportedOperationException("NO IMPLEMENTADO");
+	};
 }
