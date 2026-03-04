aebb26d75a18d8f28ddac44d9b70bba78f853042
'Javier <'javier@eskura21.com'>
2026-03-04 10:25:39 +0100

    Bibliotecas copiadas del otro proyecto

---
+++ ipartex/src/main/java/com/ipartek/formacion/ejemplos/bibliotecas/dao/DaoJpa.java
@@ -0,0 +1,90 @@
+package com.ipartek.formacion.ejemplos.bibliotecas.dao;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class DaoJpa<T> implements Dao<T> {
+	private final EntityManagerFactory entityManagerFactory;
+	private final static Map<String, EntityManagerFactory> emfs = new HashMap<>();
+	
+	private final Class<T> clase;
+
+	public DaoJpa(Class<T> clase, String unidadPersistencia) {
+		this.clase = clase;
+		
+		if(emfs.containsKey(unidadPersistencia)) {
+			entityManagerFactory = emfs.get(unidadPersistencia);
+			return;
+		}
+		
+		entityManagerFactory = Persistence.createEntityManagerFactory(unidadPersistencia);
+		
+		emfs.put(unidadPersistencia, entityManagerFactory);
+	}
+
+	public <R> R ejecutarJpa(Function<EntityManager, R> funcion) {
+		EntityTransaction t = null;
+
+		try (EntityManager em = entityManagerFactory.createEntityManager()) {
+			t = em.getTransaction();
+
+			t.begin();
+
+			R resultado = funcion.apply(em);
+
+			t.commit();
+
+			return resultado;
+		} catch (Exception e) {
+			if (t != null) {
+				t.rollback();
+			}
+
+			throw new DaoException("Error en la operación de JPA", e);
+		}
+	}
+
+	
+	
+	@Override
+	public Iterable<T> obtenerTodos() {
+		return ejecutarJpa(em -> em.createQuery("from " + clase.getSimpleName(), clase).getResultList());
+	}
+
+	@Override
+	public Optional<T> obtenerPorId(Long id) {
+		return ejecutarJpa(em -> Optional.ofNullable(em.find(clase, id)));
+	}
+
+	@Override
+	public T insertar(T objeto) {
+		return ejecutarJpa(em -> {
+			em.persist(objeto);
+			return objeto;
+		});
+	}
+
+	@Override
+	public T modificar(T objeto) {
+		return ejecutarJpa(em -> {
+			em.merge(objeto);
+			return objeto;
+		});
+	}
+
+	@Override
+	public void borrar(Long id) {
+		ejecutarJpa(em -> {
+			em.remove(em.find(clase, id));
+			return null;
+		});
+	}
+
+}
