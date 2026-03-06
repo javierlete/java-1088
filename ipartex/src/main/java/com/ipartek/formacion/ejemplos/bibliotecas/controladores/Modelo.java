36a5f975845d6843c16aec737122014584d8d255
'Javier <'javier@eskura21.com'>
2026-03-06 09:46:32 +0100

    Proyecto ipartex modificado con bibliotecas como dependencia de Maven

--- ipartex/src/main/java/com/ipartek/formacion/ejemplos/bibliotecas/controladores/Modelo.java
+++
@@ -1,8 +0,0 @@
-package com.ipartek.formacion.ejemplos.bibliotecas.controladores;
-
-import java.util.Map;
-
-public record Modelo(Map<String, String[]> entrada, Map<String, Object> salida,
-		Map<String, Object> sesionEntrada, Map<String, Object> sesionSalida) {
-
-}
