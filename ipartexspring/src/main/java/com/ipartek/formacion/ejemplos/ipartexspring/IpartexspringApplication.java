4db9b627199d67fb5fd39a48caa8449df5eb5bd3
'Javier <'javier@eskura21.com'>
2026-03-09 09:05:41 +0100

    ipartexspring: Spring Boot Starter: JPA, Validation

---
+++ ipartexspring/src/main/java/com/ipartek/formacion/ejemplos/ipartexspring/IpartexspringApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ejemplos.ipartexspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpartexspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpartexspringApplication.class, args);
+	}
+
+}
---
+++ ipartexspring/src/test/java/com/ipartek/formacion/ejemplos/ipartexspring/IpartexspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ejemplos.ipartexspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpartexspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
