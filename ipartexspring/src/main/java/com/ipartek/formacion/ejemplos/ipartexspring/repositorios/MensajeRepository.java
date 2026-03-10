0f09da84a7b296f1c14c5acb4322e5cc3df551f8
'Javier <'javier@eskura21.com'>
2026-03-10 08:48:03 +0100

    Añadimos Spring Data REST (Rest Repositories y HAL Explorer)

--- ipartexspring/src/main/java/com/ipartek/formacion/ejemplos/ipartexspring/repositorios/MensajeRepository.java
+++ ipartexspring/src/main/java/com/ipartek/formacion/ejemplos/ipartexspring/repositorios/MensajeRepository.java
@@ -2,9 +2,11 @@ package com.ipartek.formacion.ejemplos.ipartexspring.repositorios;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ipartek.formacion.ejemplos.ipartexspring.entidades.Mensaje;
 
+@RepositoryRestResource(path = "mensajes", collectionResourceRel = "mensajes")
 public interface MensajeRepository extends CrudRepository<Mensaje, Long>, PagingAndSortingRepository<Mensaje, Long> {
 
 }
