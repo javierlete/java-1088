ab0e82da1a7a8abc11740b1820eda8a83106c547
Javier <javier@eskura21.com>
2026-03-27 09:08:18 +0100

    Rutado con home llevando a details

--- FRONTEND/ANGULAR/tutorial/src/app/app.routes.ts
+++ FRONTEND/ANGULAR/tutorial/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Details } from './details/details';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+    title: 'Home page',
+  },
+  {
+    path: 'details/:id',
+    component: Details,
+    title: 'Home details',
+  },
+];
+
