0ef0173f30c111cf3389adde5a6db6d06ac9137a
Javier <javier@eskura21.com>
2026-03-27 09:44:30 +0100

    Búsqueda por ciudad

--- FRONTEND/ANGULAR/tutorial/src/app/home/home.ts
+++ FRONTEND/ANGULAR/tutorial/src/app/home/home.ts
@@ -12,8 +12,21 @@ import { HousingLocation } from "../housing-location/housing-location";
 export class Home {
   housingLocationList: HousingLocationInfo[] = [];
   housingService: HousingService = inject(HousingService);
-  
+
+  filteredLocationList: HousingLocationInfo[] = [];
+
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
   }
 }
