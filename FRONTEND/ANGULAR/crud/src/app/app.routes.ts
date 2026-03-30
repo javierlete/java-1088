d809ee88d065b1c16bd6d90440a75466f2d5d19b
Javier <javier@eskura21.com>
2026-03-30 09:04:24 +0200

    Routing para los dos componentes

--- FRONTEND/ANGULAR/crud/src/app/app.routes.ts
+++ FRONTEND/ANGULAR/crud/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Listado } from './listado/listado';
+import { Formulario } from './formulario/formulario';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Listado },
+    { path: 'formulario', component: Formulario },
+    { path: 'formulario/:id', component: Formulario },
+];
--- FRONTEND/ANGULAR/crud/src/app/listado/listado.ts
+++ FRONTEND/ANGULAR/crud/src/app/listado/listado.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-listado',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './listado.html',
   styleUrl: './listado.css',
 })
