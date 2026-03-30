16aa6fe360877abced19c8d424eaf6132030f38f
Javier <javier@eskura21.com>
2026-03-30 08:40:34 +0200

    Listado estático

--- FRONTEND/ANGULAR/crud/src/app/app.ts
+++ FRONTEND/ANGULAR/crud/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Listado } from "./listado/listado";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Listado],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
