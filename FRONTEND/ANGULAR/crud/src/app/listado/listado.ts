e64daebf3fa3c0cf552140a6f65f3e95bb89781f
Javier <javier@eskura21.com>
2026-03-30 08:33:54 +0200

    ng g c listado

---
+++ FRONTEND/ANGULAR/crud/src/app/listado/listado.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  imports: [],
+  templateUrl: './listado.html',
+  styleUrl: './listado.css',
+})
+export class Listado {}
