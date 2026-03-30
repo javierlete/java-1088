0173bff2d43f2399626ca4509486983079110a5b
Javier <javier@eskura21.com>
2026-03-30 10:21:00 +0200

    Formulario con datos recibidos por el id de la URL

--- FRONTEND/ANGULAR/crud/src/app/formulario/formulario.ts
+++ FRONTEND/ANGULAR/crud/src/app/formulario/formulario.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from "@angular/router";
+import { PRODUCTOS } from '../mock-productos';
+import { Producto } from '../producto';
 
 @Component({
   selector: 'app-formulario',
-  imports: [RouterLink],
+  imports: [FormsModule],
   templateUrl: './formulario.html',
   styleUrl: './formulario.css',
 })
-export class Formulario {}
+export class Formulario {
+  producto: Producto = {} as Producto;
+
+  private readonly route: ActivatedRoute = inject(ActivatedRoute);
+  private readonly router: Router = inject(Router);
+
+  constructor() {
+    const id: number = +this.route.snapshot.params['id'];
+
+    console.log('formulario', id);
+
+    this.producto = PRODUCTOS.find(p => p.id === id)!;
+  }
+
+  guardar(): void {
+    console.log('guardar', this.producto);
+
+    this.router.navigate(['']);
+  }
+}
