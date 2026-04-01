bde9e8f33e8f72a9d11bd981d43367de937a25e8
Javier <javier@eskura21.com>
2026-04-01 10:49:28 +0200

    Angular Tour of Heroes hasta servicios sin navegación

--- FRONTEND/ANGULAR/angular-tour-of-heroes/src/app/app-module.ts
+++ FRONTEND/ANGULAR/angular-tour-of-heroes/src/app/app-module.ts
@@ -3,18 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, HeroesComponent, HeroDetailComponent, MessagesComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
