1beb5ab58157ef82cbc8cdaabf2678221e0ee20f
Javier <javier@eskura21.com>
2026-04-08 09:38:43 +0200

    Formulario de búsqueda con autocompletado

--- FRONTEND/ANGULAR/angular-tour-of-heroes/src/app/app-module.ts
+++ FRONTEND/ANGULAR/angular-tour-of-heroes/src/app/app-module.ts
@@ -9,9 +9,17 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
-  declarations: [App, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent],
+  declarations: [
+    App,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroSearchComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
