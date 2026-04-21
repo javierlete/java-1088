b8d1f55d17bcb8c36d0b9a1d986ba25b6d8d8d0a
Javier <javier@eskura21.com>
2026-04-21 11:14:33 +0200

    UsuarioService ahora usa el servicio REST real

--- iparfood-frontend/src/app/usuario-service.ts
+++ iparfood-frontend/src/app/usuario-service.ts
@@ -1,6 +1,7 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { Usuario } from './usuario';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, of, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -8,33 +9,36 @@ import { Observable, of } from 'rxjs';
 export class UsuarioService {
   usuario = signal<Usuario | undefined>(undefined);
 
+  private readonly http = inject(HttpClient);
+  private readonly url = 'http://localhost:8080/api/v2/login';
+
   constructor() {
     // Al iniciar la app, restauramos el usuario desde sessionStorage
     const raw = sessionStorage.getItem('usuario');
-    
+
     if (raw) {
       this.usuario.set(JSON.parse(raw));
     }
   }
 
   autenticar(usuario: Usuario): Observable<Usuario | undefined> {
-    if (usuario.email === 'javier@email.net' && usuario.password === 'javier') {
-      usuario.nombre = 'Javier';
-      usuario.password = '';
-
-      this.usuario.set(usuario);
-      sessionStorage.setItem('usuario', JSON.stringify(usuario));
-
-      return of(usuario);
-    } else {
-      return of(undefined);
-    }
+    return this.http.post<Usuario>(this.url, usuario).pipe(
+      tap(usuarioLogin => {
+        console.log(usuarioLogin);
+        this.usuario.set(usuarioLogin);
+        sessionStorage.setItem('usuario', JSON.stringify(usuarioLogin));
+      }),
+      catchError(err => {
+        console.error(err);
+        return of(undefined)
+      })
+    );
   }
 
   logout(): Observable<any> {
     this.usuario.set(undefined);
     sessionStorage.removeItem('usuario');
-    
+
     return of(null);
   }
 }
