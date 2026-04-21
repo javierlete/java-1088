e8db842ac0f0593f70cfda247b47535237abe552
Javier <javier@eskura21.com>
2026-04-21 11:15:19 +0200

    Usamos signals para el error y la alerta se puede cerrar y volverá a aparecer si es necesario

--- iparfood-frontend/src/app/login/login.ts
+++ iparfood-frontend/src/app/login/login.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Usuario } from '../usuario';
 import { FormsModule } from '@angular/forms';
 import { UsuarioService } from '../usuario-service';
@@ -11,7 +11,7 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrl: './login.css',
 })
 export class Login {
-  errorLogin = false;
+  errorLogin = signal(false);
   cierreSesion = false;
 
   usuario: Usuario = {
@@ -26,15 +26,17 @@ export class Login {
   constructor() {
     this.cierreSesion = Boolean(this.route.snapshot.queryParamMap.get('logout'));
   }
-  
+
   envioFormulario() {
-    console.log(this.usuario);
+    console.log('Credenciales enviadas', this.usuario);
 
     this.usuarioService.autenticar(this.usuario).subscribe(
-      usuario => { 
-        this.errorLogin = !usuario;
-        
-        if(usuario) {
+      usuario => {
+        console.log('Usuario recibido', usuario);
+
+        this.errorLogin.set(!usuario);
+
+        if (usuario) {
           this.router.navigate(['/']);
         }
       }
