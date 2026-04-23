47d49216d5d284d12bade1e109bb0b7118ce2573
Javier <javier@eskura21.com>
2026-04-23 09:03:49 +0200

    El servicio REST obtiene los datos del servicio de Spring

--- iparfood/src/main/java/com/ipartek/formacion/ejemplos/iparfood/rest/TiposComidaRestController.java
+++ iparfood/src/main/java/com/ipartek/formacion/ejemplos/iparfood/rest/TiposComidaRestController.java
@@ -1,12 +1,11 @@
 package com.ipartek.formacion.ejemplos.iparfood.rest;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ipartek.formacion.ejemplos.iparfood.entidades.TipoComida;
+import com.ipartek.formacion.ejemplos.iparfood.servicios.AdministradorService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,9 +14,10 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/v2/tipos-comida")
 public class TiposComidaRestController {
+	private final AdministradorService administradorService;
+	
 	@GetMapping
 	public Iterable<TipoComida> get() {
-		return List.of(TipoComida.builder().id(1L).nombre("Uno").build(),
-				TipoComida.builder().id(2L).nombre("Dos").build());
+		return administradorService.listarTiposComida();
 	}
 }
